10a7f306e9616598be14efba6eeea926761e99c0
zealptl <zealpatel06@gmail.com>
2021-01-07 18:44:03 -0500

    make contact form functional

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
 import {
   TextField,
   Button,
@@ -32,16 +33,64 @@ const CustomTextField = withStyles({
 
 const ContactForm = () => {
   const classes = useStyles();
+
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    message: '',
+  });
+
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    console.log(formData);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .send(
+        'service_tttmavr',
+        'template_e6d0z59',
+        {
+          to_email: 'zealpatel@gmail.com',
+          from_name: `${formData.firstName} ${formData.lastName}`,
+          from_email: formData.email,
+          message: formData.message,
+        },
+        'user_b3R2pRza2POSIG7iBB20q'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    setFormData({
+      firstName: '',
+      lastName: '',
+      email: '',
+      message: '',
+    });
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid container spacing={2}>
           <Grid item xs={6}>
             <CustomTextField
               id='firstName'
+              name='firstName'
               label='First Name'
               required
               fullWidth
+              value={formData.firstName}
+              onChange={onChange}
               InputLabelProps={{
                 classes: {
                   root: classes.cssLabel,
@@ -52,9 +101,11 @@ const ContactForm = () => {
           <Grid item xs={6}>
             <CustomTextField
               id='lastName'
+              name='lastName'
               label='Last Name'
-              required
               fullWidth
+              value={formData.lastName}
+              onChange={onChange}
               InputLabelProps={{
                 classes: {
                   root: classes.cssLabel,
@@ -65,22 +116,12 @@ const ContactForm = () => {
           <Grid item xs={12}>
             <CustomTextField
               id='email'
+              name='email'
               label='Email'
               required
               fullWidth
-              InputLabelProps={{
-                classes: {
-                  root: classes.cssLabel,
-                },
-              }}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <CustomTextField
-              id='subject'
-              label='Subject'
-              required
-              fullWidth
+              value={formData.email}
+              onChange={onChange}
               InputLabelProps={{
                 classes: {
                   root: classes.cssLabel,
@@ -91,11 +132,14 @@ const ContactForm = () => {
           <Grid item xs={12}>
             <CustomTextField
               id='message'
+              name='message'
               label='Message'
               multiline
               rows={4}
               required
               fullWidth
+              value={formData.message}
+              onChange={onChange}
               InputLabelProps={{
                 classes: {
                   root: classes.cssLabel,
