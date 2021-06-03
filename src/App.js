f6445235df6905f61e54dc3b1ea58a381d8ad6f1
zealptl <zealpatel06@gmail.com>
2021-06-03 14:24:11 -0400

    update view prohject link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import './Queries.css';
+import './styles/app.css';
+import './styles/appQueries.css';
 import { ThemeProvider } from '@material-ui/core';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // import AnimatedCursor from 'react-animated-cursor';
@@ -9,12 +9,12 @@ import HomePage from './pages/HomePage';
 import ProjectPages from './pages/ProjectPages';
 
 const App = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <Switch>
-          <div className='App'>
-            {/* <AnimatedCursor
+	return (
+		<ThemeProvider theme={theme}>
+			<Router>
+				<Switch>
+					<div className='App'>
+						{/* <AnimatedCursor
               innerSize={15}
               outerSize={15}
               color='230, 146, 161'
@@ -22,13 +22,13 @@ const App = () => {
               innerScale={1}
               outerScale={3}
             /> */}
-            <Route exact path='/' component={HomePage} />
-            <Route path='/projects' component={ProjectPages} />
-          </div>
-        </Switch>
-      </Router>
-    </ThemeProvider>
-  );
+						<Route exact path='/' component={HomePage} />
+						<Route path='/projects' component={ProjectPages} />
+					</div>
+				</Switch>
+			</Router>
+		</ThemeProvider>
+	);
 };
 
 export default App;
