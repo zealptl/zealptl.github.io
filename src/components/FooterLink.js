57a1852f0e1af315dad616cc90ae0fae1c15e411
zealptl <zealpatel06@gmail.com>
2021-06-03 12:02:17 -0400

    update footer link

--- src/components/FooterLink.js
+++ src/components/FooterLink.js
@@ -1,24 +1,24 @@
 import React from 'react';
 
 const FooterLink = (props) => {
-  const Icon = props.icon;
-  return props.download ? (
-    <a
-      href={props.to}
-      target='_blank'
-      rel='noreferrer'
-      download={props.download}
-      className='footer-link'
-    >
-      <Icon className='footer-icon' />
-      <p>{props.heading}</p>
-    </a>
-  ) : (
-    <a href={props.to} target='_blank' rel='noreferrer' className='footer-link'>
-      <Icon />
-      <p>{props.heading}</p>
-    </a>
-  );
+	const Icon = props.icon;
+	return props.download ? (
+		<a
+			href={props.to}
+			target='_blank'
+			rel='noreferrer'
+			download={props.download}
+			className='footer-link'
+		>
+			<Icon className='footer-icon' />
+			<p>{props.heading}</p>
+		</a>
+	) : (
+		<a href={props.to} target='_blank' rel='noreferrer' className='footer-link'>
+			<Icon className='footer-icon' />
+			<p>{props.heading}</p>
+		</a>
+	);
 };
 
 export default FooterLink;
