6f476b823ba7a95fc04d678d98a0fa322fa71922
zealptl <zealpatel06@gmail.com>
2021-07-24 18:18:32 -0400

    change theme & logo

--- src/theme.js
+++ src/theme.js
@@ -1,30 +1,30 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#A7CECB',
-      dark: '#8CADAB',
-    },
-    secondary: {
-      main: '#E692A1',
-      dark: '#B4737F',
-    },
-    tertiary: {
-      main: '#9D9FAA',
-      dark: '#2D2D2D',
-      light: '#F7F7F7',
-    },
-    background: {
-      light: '#fff',
-      dark: '#333',
-    },
-    font: {
-      dark: '#000',
-      light: '#fff',
-    },
-  },
-  // add more css properties as desired
+	palette: {
+		primary: {
+			main: '#457B9D',
+			dark: '#8CADAB',
+		},
+		secondary: {
+			main: '#E63946',
+			dark: '#B4737F',
+		},
+		tertiary: {
+			main: '#9D9FAA',
+			dark: '#2D2D2D',
+			light: '#F7F7F7',
+		},
+		background: {
+			light: '#fff',
+			dark: '#333',
+		},
+		font: {
+			dark: '#000',
+			light: '#fff',
+		},
+	},
+	// add more css properties as desired
 });
 
 export default theme;
